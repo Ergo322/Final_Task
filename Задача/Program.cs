1e94d5736e63b5adb8ebda17b6bb56b68d0e9697
Ergo322 <Shikmen2@mail.ru>
2022-10-04 22:45:36 +0300

    основная функция

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] name = { "Alex", "Lex", "Tom", "Andrey", "Yn", "Jo", "Jon", "Jony", "Tony"  };
 string[] SymbolThree = new string [name.Length];
+
+void SearchName(string[] name, string[] SymbolThree)
+{
+    int count = 0;
+    for (int i = 0; i < name.Length; i++)
+    {
+        if (name[i].Length <= 3)
+        {
+            SymbolThree[count] = name[i];
+            count++;
+        }
+    }
+}
